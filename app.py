8fe14fc61d15857bc20c99f56feba47b2bcc16c0
Ahmet Şafak Yıldırım <114856193+ahm3texe@users.noreply.github.com>
2025-05-10 23:06:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ with gr.Blocks(
 
 def launch_gradio(server_name: str, server_port: int) -> None:
     Applio.launch(
-        favicon_path="assets/ICON.ico",
+        favicon_path="assets/favicon.ico",
         share="--share" in sys.argv,
         inbrowser="--open" in sys.argv,
         server_name=server_name,
